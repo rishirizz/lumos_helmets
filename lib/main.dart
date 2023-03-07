dbdd041f70f81024f21e20930a07298f9564f912
arijeet <arijeet@sbox.ltd>
2023-03-07 17:48:24 +0530

    added folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lumos_helmets/pages/my_helmets_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHelmetsPage(),
     );
   }
 }
-
